a29331e66cc4d4b7f57d164f4e52d2660b8be5ee
florian5363 <81989282+florian5363@users.noreply.github.com>
2026-02-08 13:10:03 +0100

    Delete setup.py
    temporarily moved to main.py

--- setup.py
+++
@@ -1,4 +0,0 @@
-with open("data.txt", "w") as file:
-    file.write(input("Enter the Server url\n>> ")+"\n")
-    file.write(input("Enter the Api key\n>> ")+"\n")
-    file.write(input("Enter the Username\n>> ")+"\n")
